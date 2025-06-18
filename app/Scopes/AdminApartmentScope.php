be1832cd28e563bb40b6cd22aeaf7eb3088c4765
dodod <your-uwambadodo@gmail.com>
2025-06-18 23:01:01 +0200

    admin authentications

---
+++ app/Scopes/AdminApartmentScope.php
@@ -0,0 +1,25 @@
+<?php
+// App\Scopes\AdminHotelScope.php
+namespace App\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Support\Facades\Auth;
+
+class AdminApartmentScope implements Scope
+{
+    public function apply(Builder $builder, Model $model)
+    {
+        if (Auth::guard('admin')->check()) {
+            $admin = Auth::guard('admin')->user();
+
+            // Get the list of hotel_ids the admin manages
+            $hotelIds = $admin->manages()
+                ->where('object', 'apartment')
+                ->pluck('object_id');
+
+            $builder->whereIn('apartment_id', $hotelIds);
+        }
+    }
+}
