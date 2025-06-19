e05a810357e91e16330446df1bbd59eb3aab8e0e
gapetitheos@gmail.com <uwambadodo@gmail.com>
2025-06-19 11:25:28 +0200

    pricing

--- app/Rest/Controllers/ApartmentController.php
+++ app/Rest/Controllers/ApartmentController.php
@@ -108,6 +108,8 @@ class ApartmentController extends RestController
         'gym'                => 'nullable|boolean',
         'room_service'       => 'nullable|boolean',
         'sauna_massage'      => 'nullable|boolean',
+        'price_per_night'    => 'required|numeric',
+        'price_per_month'    => 'required|numeric',
         'apartment_owner_id' => 'required|exists:apartment_owners,id',
         'contact'    => 'nullable|string|max:20', // Assuming contact number is optional
 
@@ -227,6 +229,8 @@ class ApartmentController extends RestController
             'laundry' => 'nullable|boolean',
             'gym' => 'nullable|boolean',
             'room_service' => 'nullable|boolean',
+            'price_per_night' => 'required|numeric',
+            'price_per_month'    => 'required|numeric',
             'sauna_massage' => 'nullable|boolean',
         ]);
 
