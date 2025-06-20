c11ced12d05c6bcc4518cc1f404621800a1c3953
dodod <your-uwambadodo@gmail.com>
2025-06-20 15:24:48 +0200

    fixing romm list

--- resources/views/emails/contact-submitted.blade.php
+++ resources/views/emails/contact-submitted.blade.php
@@ -2,6 +2,7 @@
 
 <p><strong>Name:</strong> {{ $contact->name }}</p>
 <p><strong>Phone:</strong> {{ $contact->phone }}</p>
+<p><strong>Issue:</strong> {{ $contact->email }}</p>
 <p><strong>Address:</strong> {{ $contact->address }}</p>
 <p><strong>Issue:</strong> {{ $contact->issue }}</p>
 <p><strong>Category:</strong> {{ $contact->category }}</p>
